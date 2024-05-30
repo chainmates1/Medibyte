95f01a89f9c503ca87185e6a9e4b644eb6f36d8a
Hephaestus-V <varadbardapurkar@gmail.com>
2024-05-30 14:54:01 +0530

    Added CCIP feature in our project

--- contracts/Health_Contract.sol
+++ contracts/Health_Contract.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./MediCoin.sol";
@@ -12,13 +14,22 @@ import "./HealthInsuranceNFT.sol";
 import "./FreeHealthKitNFT.sol";
 import "./FreeHealthCheckupNFT.sol";
 
-contract Health_Contract is FunctionsClient, ConfirmedOwner {
+
+
+interface IHealth_Contract {
+    function selectTests(address _patient,uint8[] calldata selectedTests,uint256 _amount) external;
+}
+
+contract Health_Contract is IHealth_Contract,FunctionsClient, ConfirmedOwner {
     using FunctionsRequest for FunctionsRequest.Request;
     using Strings for uint256;
     using Strings for address;
+    using SafeERC20 for IERC20;
+
 
     PatientNFT public patientNFT;
     MediCoin public healthToken;
+    IERC20 private immutable i_usdcToken;
     HealthInsuranceNFT public healthInsuranceNFT;
     FreeHealthKitNFT public freeHealthKitNFT;
     FreeHealthCheckupNFT public freeHealthCheckupNFT;
@@ -31,13 +42,14 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
     error UnexpectedRequestID(bytes32 requestId);
     error PatientNotFound(address patient);
     error InvalidReferral(address referrer);
+    error InvalidUsdcToken(); // Used when the usdc token address is 0
     error InsufficientBalance(uint256 required, uint256 available);
 
     event Response(bytes32 indexed requestId, bytes response, bytes err);
     event HealthScoreUpdated(uint256 tokenId, uint256 healthScore, uint256 tokensEarned);
     event RequestInitiated(address doctor, uint256 tokenId, uint256 timestamp);
     event ReferralRegistered(address referrer, address referee);
-    event TestsSelected(address indexed patient, uint8[] selectedTests); // Added indexed modifier for patient address
+    event TestsSelected(address patient, uint8[] selectedTests);
     event TestsCleared(address patient);
     event RewardClaimed(address patient, address rewardContract, uint256 tokenId);
 
@@ -48,7 +60,6 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
     mapping(address => uint8[10]) public patientTests; // Assuming we have 10 tests
     mapping(address => uint256) public rewardThresholds;
 
-
     uint256 public referralReward = 100 * 10 ** 18; // Example referral reward in tokens
 
     constructor(
@@ -57,13 +68,16 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
         address _healthToken,
         address _healthInsuranceNFT,
         address _freeHealthKitNFT,
-        address _freeHealthCheckupNFT
+        address _freeHealthCheckupNFT,
+        address _usdcToken
     ) FunctionsClient(router) ConfirmedOwner(msg.sender) {
         patientNFT = PatientNFT(_patientNFT);
         healthToken = MediCoin(_healthToken);
         healthInsuranceNFT = HealthInsuranceNFT(_healthInsuranceNFT);
         freeHealthKitNFT = FreeHealthKitNFT(_freeHealthKitNFT);
         freeHealthCheckupNFT = FreeHealthCheckupNFT(_freeHealthCheckupNFT);
+         if (_usdcToken == address(0)) revert InvalidUsdcToken();
+        i_usdcToken = IERC20(_usdcToken);
     }
 
     // Modifier to check if the caller is an authorized doctor
@@ -94,10 +108,10 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
     }
 
     // Function for a patient to select and submit tests
-    function selectTests(uint8[] calldata selectedTests) external {
+    function selectTests(address _patient,uint8[] calldata selectedTests,uint256 _amount) external {
         require(selectedTests.length <= 10, "Too many tests selected");
-        
-        uint8[10] storage tests = patientTests[msg.sender];
+        i_usdcToken.transferFrom(msg.sender,address(this), _amount);
+        uint8[10] storage tests = patientTests[_patient];
         for (uint8 i = 0; i < 10; i++) {
             tests[i] = 0; // Reset all tests
         }
@@ -106,7 +120,7 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
             tests[selectedTests[i]] = 1; // Mark selected tests
         }
 
-        emit TestsSelected(msg.sender, selectedTests); // Emit event with patient's address
+        emit TestsSelected(_patient, selectedTests);
     }
 
     // Function to set reward thresholds
@@ -152,19 +166,21 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
         address patient,
         uint256 amountPaid
     ) external onlyAuthorizedDoctor {
-        
-        
         uint256 tokenId = patientNFT.getTokenIdByPatient(patient);
         string memory uri = "";
 
+        // Check if the patient already has an NFT
         if (tokenId != 0) {
             uri = patientNFT.tokenURI(tokenId);
         }
 
+        // Fetch the balance of health tokens for the patient
         uint256 tokenBalance = healthToken.balanceOf(patient);
 
+        // Log the transaction details
         emit RequestInitiated(msg.sender, tokenId, block.timestamp);
 
+        // Initialize and send the request
         FunctionsRequest.Request memory req;
         req.initializeRequestForInlineJavaScript(source);
         if (encryptedSecretsUrls.length > 0) {
@@ -173,20 +189,23 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
             req.addDONHostedSecrets(donHostedSecretsSlotID, donHostedSecretsVersion);
         }
 
+        // Set arguments for the request
         string[] memory args = new string[](4);
         args[0] = uri;
-        args[1] = patient.toHexString();
-        args[2] = amountPaid.toString();
-        args[3] = tokenBalance.toString();
+        args[1] = Strings.toHexString(patient);
+        args[2] = Strings.toString(amountPaid);
+        args[3] = Strings.toString(tokenBalance);
 
         req.setArgs(args);
 
         s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, gasLimit, donID);
 
+        // Map the request ID to the token ID and patient address
         requestIdToTokenId[s_lastRequestId] = tokenId;
         requestIdToPatient[s_lastRequestId] = patient;
     }
 
+   
     function fulfillRequest(
         bytes32 requestId,
         bytes memory response,
@@ -205,14 +224,17 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
             uint256 tokenId = requestIdToTokenId[requestId];
             address patient = requestIdToPatient[requestId];
 
+            // If the patient doesn't have an NFT, mint one with the provided URI
             if (tokenId == 0) {
                 patientNFT.safeMint(patient, newUri);
                 tokenId = patientNFT.getTokenIdByPatient(patient);
             } else {
+                // Update the existing NFT with the new URI
                 patientNFT.setTokenURI(tokenId, newUri);
             }
 
             healthToken.mint(patient, tokensEarned);
+
             // Handle referral reward
             address referrer = referrals[patient];
             if (referrer != address(0)) {
@@ -220,12 +242,14 @@ contract Health_Contract is FunctionsClient, ConfirmedOwner {
                 healthToken.mint(patient, referralReward);
             }
 
-            emit Response(requestId, response, err);
+            // Clear the test selections for the patient
             delete patientTests[patient];
             emit TestsCleared(patient);
             emit HealthScoreUpdated(tokenId, healthScore, tokensEarned);
         }
 
-        
+        emit Response(requestId, response, err);
     }
+
+    
 }
