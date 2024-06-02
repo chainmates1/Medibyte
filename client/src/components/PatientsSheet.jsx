4858a45a393c988e57138c7e9196dfb08992b19c
Dasari Manoj <122810482+Manudasari265@users.noreply.github.com>
2024-06-02 16:36:49 +0530

    Fixed the UI/UX of rewards page

--- client/src/components/PatientsSheet.jsx
+++ client/src/components/PatientsSheet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 import abi from "../abis/Health_Contract.json";
